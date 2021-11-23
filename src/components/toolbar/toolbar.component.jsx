26d4d91ebe3db2ce117a8555834acb3ef794a379
JamesGabby <jlghfe36@gmail.com>
2021-11-23 01:21:29 +0000

    Styling toolbar

--- src/components/toolbar/toolbar.component.jsx
+++ src/components/toolbar/toolbar.component.jsx
@@ -8,18 +8,18 @@ export function Toolbar(props) {
     const isDark = props.mode;
     if (isDark) { 
         return (
-            <div className="App-header">
+            <div className="App-header" style={{backgroundImage: 'linear-gradient( 136deg, rgba(0,0,0,0.65) 12.9%, rgba(0,0,0,1) 145.4% )'}}>
                 <img src={logo} className="App-logo" alt="logo" />
                 <p className="Portfolio-text" style={{color: '#49fb35'}}>Portfolio</p>
                 <h1 style={{color: '#F4F6F0'}}>James Gabbitus</h1>
                 <button className="Btn-light" title="Light Mode" onClick={props.handleClick}>
                 <BsFillSunFill color='yellow'></BsFillSunFill>
-            </button>   
+                </button>   
             </div>
         );
     }
     return (
-        <div className="App-header">
+        <div className="App-header" style={{backgroundImage: 'linear-gradient( 112.1deg,  rgba(32,38,57,1) 11.4%, rgba(63,76,119,1) 70.2% )'}}>
             <img src={logo} className="App-logo" alt="logo" />
             <p className="Portfolio-text" style={{color: '#49fb35'}}>Portfolio</p>
             <h1 style={{color: '#F4F6F0'}}>James Gabbitus</h1>
