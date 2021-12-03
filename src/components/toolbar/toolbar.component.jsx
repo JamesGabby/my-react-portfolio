14dd192f27def081a81e11163395e20a6fe19cd2
JamesGabby <jlghfe36@gmail.com>
2021-12-03 17:14:17 +0000

    added hooks to header

--- src/components/toolbar/toolbar.component.jsx
+++ src/components/toolbar/toolbar.component.jsx
@@ -1,15 +1,20 @@
-import React from "react";
+import React, { useEffect, useState }  from "react";
 import './toolbar.styles.css';
 import logo from './logo.svg';
 import { FaMoon } from 'react-icons/fa';
 import { BsFillSunFill } from 'react-icons/bs'
 
 export function Toolbar(props) {
+    const [toolbar, setToolbar] = useState(false);
+    const reSize = () => {
+        window.scrollY >= 200 ? setToolbar(true) : setToolbar(false)
+    }
+    window.addEventListener('scroll', reSize);
 
     const isDark = props.mode;
     if (isDark) { 
         return (
-            <div id="App-header-dark">
+            <div class={toolbar ? 'App-header-dark active' : 'App-header-dark'}>
                 <img src={logo} className="App-logo" alt="logo" />
                 <p className="Portfolio-text" style={{color: '#49fb35'}}>Portfolio</p>
                 <h1 className="Name-text" style={{color: '#49fb35'}}>James Gabbitus</h1>
@@ -20,7 +25,7 @@ export function Toolbar(props) {
         );
     }
     return (
-        <div id="App-header-light" >
+        <div class={toolbar ? 'App-header-light active' : 'App-header-light'}>
             <img src={logo} className="App-logo" alt="logo" />
             <p className="Portfolio-text" style={{color: 'white'}}>Portfolio</p>
             <h1 className="Name-text" style={{color: 'white'}}>James Gabbitus</h1>
